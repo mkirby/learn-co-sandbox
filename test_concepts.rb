662dcfda5c8618711a9b87793830a3c0362b5537
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-09-14 12:40:43 +0000

    Automatically backed up by Learn

--- test_concepts.rb
+++ test_concepts.rb
@@ -13,3 +13,16 @@ def greeting(name = "neighbor", language = "web")
   puts "Hello #{name}. We hear you are a great #{language} programmer."
 end
 greeting("Paul")
+
+hash_list = {
+  :symbol_one => "Key Value",
+  :symbol_two => "Another Key Value",
+  "test string" => "string key value"
+}
+
+puts hash_list
+
+5.times do |parameter_set_by_the_block|
+  #every time .times runs it outputs to the parameter 0,1,2,3,etc
+  puts parameter_set_by_the_block
+end
